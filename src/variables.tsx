00e96da4f6f97bce0732ca7b26f04e53bfff880d
RenattoGuzman <renttogs@gmail.com>
2025-11-11 05:24:43 +0000

    prod changes

--- src/variables.tsx
+++ src/variables.tsx
@@ -1,2 +1,3 @@
-export const API_LINK = 'http://localhost:8000'
+//export const API_LINK = 'http://localhost:8000'
 //export const API_LINK = 'https://proyectograd-bck.onrender.com'
+export const API_LINK = 'https://tatto404.lat/api'
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,           // listen on 0.0.0.0
+    port: 5173,
+    allowedHosts: ['tatto404.lat', 'www.tatto404.lat'],
+    https: false          // remove HTTPS
+  },
 });
+
